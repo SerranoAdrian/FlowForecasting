6bd3213945b13ae960bc2b3bf5c28e3ba256882b
AdrianSerrano <adrisera14@gmail.com>
2025-02-03 10:40:47 +0100

    add uncertainty notebook

--- gnn_model.py
+++ gnn_model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch_geometric.nn.conv import GCNConv
+import torch.nn.functional as F
 
 
 class StationFlowGNN(nn.Module):
@@ -12,8 +13,16 @@ class StationFlowGNN(nn.Module):
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
+    # def forward(self, data):
+    #     x, edge_index = data.x, data.edge_index
+    #     # Propagation à travers les couches GNN
+    #     x = self.relu(self.conv1(x, edge_index))  # Première couche GCN
+    #     x = self.relu(self.conv2(x, edge_index))  # Deuxième couche GCN
+    #     # Projection sur une seule dimension pour chaque nœud (flux de passagers)
+    #     x = self.fc(x)
+    #     return self.sigmoid(x)  # Sortie de dimension [N, 1], où N est le nombre de nœuds
+    
+    def forward(self, x, edge_index):
         # Propagation à travers les couches GNN
         x = self.relu(self.conv1(x, edge_index))  # Première couche GCN
         x = self.relu(self.conv2(x, edge_index))  # Deuxième couche GCN
@@ -49,3 +58,17 @@ class InterStationFlowGNN(nn.Module):
         edge_predictions = self.edge_mlp(edge_features)  # [E, output_dim]
 
         return edge_predictions
+    
+
+# Définition du modèle GCN avec régression quantile
+class QuantileGCN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, quantiles=[0.05, 0.5, 0.95]):
+        super().__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, len(quantiles))  # 1 sortie par quantile
+        self.quantiles = quantiles
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = self.conv2(x, edge_index)
+        return x  # Retourne [nœuds, quantiles]
