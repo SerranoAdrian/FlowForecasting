9b490d6fa9c7ffb97df52d87ef0ee493dc6edc41
AdrianSerrano <adrisera14@gmail.com>
2025-02-10 16:09:22 +0100

    Create data folder and new model tests

--- gnn_model.py
+++ gnn_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch_geometric.nn.conv import GCNConv, GATv2Conv
 from torch_geometric.nn import Sequential
+from torch_geometric.nn import APPNP
 
 
 class StationFlowGCN(nn.Module):
@@ -118,3 +119,179 @@ class QuantileGCN(torch.nn.Module):
         x = self.conv1(x, edge_index)
         x = self.conv2(x, edge_index)
         return x  # Retourne [nœuds, quantiles]
+    
+
+
+
+class StationFlowGCN2(nn.Module):
+    def __init__(self, num_nodes : int, input_dim: int, output_dim: int, embeddings=None, freeze=True):
+        super(StationFlowGCN2, self).__init__()
+
+        self.use_embeddings = False if embeddings is None else True
+        
+        if self.use_embeddings:
+            if isinstance(embeddings, torch.Tensor):
+                self.node_emb = nn.Embedding.from_pretrained(embeddings, freeze=freeze)
+            elif embeddings == 'initialized':
+                self.node_emb = nn.Embedding(num_nodes, input_dim)
+            else:
+                raise(Exception)
+        
+        self.gcn = Sequential('x, edge_index, edge_weight',
+            [(GCNConv(input_dim, 256), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(256, 128), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(128, 64), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(64, 64), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(64, 32), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(32, 32), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(32, output_dim), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU()],
+        )
+
+    def forward(self,x, edge_index, edge_weight=None):
+        
+        if self.use_embeddings:
+            x = self.node_emb(x)
+
+        x = self.gcn(x, edge_index, edge_weight)
+        return x
+    
+
+
+
+class StationFlowGCN3(nn.Module):
+    def __init__(self, num_nodes : int, input_dim: int, output_dim: int, embeddings=None, freeze=True):
+        super(StationFlowGCN3, self).__init__()
+
+        self.use_embeddings = False if embeddings is None else True
+        
+        if self.use_embeddings:
+            if isinstance(embeddings, torch.Tensor):
+                self.node_emb = nn.Embedding.from_pretrained(embeddings, freeze=freeze)
+            elif embeddings == 'initialized':
+                self.node_emb = nn.Embedding(num_nodes, input_dim)
+            else:
+                raise(Exception)
+        
+        self.gcn = Sequential('x, edge_index, edge_weight',
+            [(GCNConv(input_dim, 16), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(16, 8), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(8, 8), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(8, output_dim), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU()],
+        )
+
+    def forward(self,x, edge_index, edge_weight=None):
+        
+        if self.use_embeddings:
+            x = self.node_emb(x)
+
+        x = self.gcn(x, edge_index, edge_weight)
+        return x
+    
+
+class DeepGCN(torch.nn.Module):
+    def __init__(self, num_nodes, input_dim, hidden_dim, output_dim, embeddings=None, freeze=True):
+        super(DeepGCN, self).__init__()
+        # self.convs = torch.nn.ModuleList()
+
+        self.use_embeddings = False if embeddings is None else True
+        
+        if self.use_embeddings:
+            if isinstance(embeddings, torch.Tensor):
+                self.node_emb = nn.Embedding.from_pretrained(embeddings, freeze=freeze)
+            elif embeddings == 'initialized':
+                self.node_emb = nn.Embedding(num_nodes, input_dim)
+            else:
+                raise(Exception)
+        
+
+        self.gcn = Sequential('x, edge_index, edge_weight',
+            [(GCNConv(input_dim, 16), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(16, 16), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(16, 16), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(16, 16), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(16, 16), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(16, 16), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(16, 16), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(16, 16), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(16, 16), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(16, 16), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(16, 16), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(16, 16), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU(),
+            (GCNConv(16, output_dim), 'x, edge_index, edge_weight -> x'),
+            nn.ReLU()],
+        )
+
+
+    def forward(self, x, edge_index, edge_weight=None):
+        if self.use_embeddings:
+            x = self.node_emb(x)
+
+        x = self.gcn(x, edge_index, edge_weight)
+        return x
+    
+
+class GraphAPPNP(torch.nn.Module):
+    def __init__(self, K, alpha, input_dim, output_dim):
+        super(GraphAPPNP, self).__init__()
+        self.K = K
+        self.alpha = alpha
+        self.appnp = Sequential('x, edge_index', [
+            (GCNConv(input_dim, 128), 'x, edge_index -> x'),
+            nn.ReLU(),
+            (APPNP(K=K, alpha=alpha), 'x, edge_index -> x'),  # Propagation longue distance
+            (GCNConv(128, output_dim), 'x, edge_index -> x')
+        ])
+
+    def forward(self, x, edge_index, edge_weight=None):
+        return self.appnp(x, edge_index)
+
+class Baseline(torch.nn.Module):
+    def __init__(self, num_nodes, input_dim, hidden_dim, output_dim, embeddings=None, freeze=True):
+        super(Baseline, self).__init__()
+        # self.convs = torch.nn.ModuleList()
+
+        self.use_embeddings = False if embeddings is None else True
+        
+        if self.use_embeddings:
+            if isinstance(embeddings, torch.Tensor):
+                self.node_emb = nn.Embedding.from_pretrained(embeddings, freeze=freeze)
+            elif embeddings == 'initialized':
+                self.node_emb = nn.Embedding(num_nodes, input_dim)
+            else:
+                raise(Exception)
+        
+
+        self.gcn = Sequential([nn.ReLU()],
+        )
+
+
+    def forward(self, x, edge_index, edge_weight=None):
+        if self.use_embeddings:
+            x = self.node_emb(x)
+
+        return x
+
+
